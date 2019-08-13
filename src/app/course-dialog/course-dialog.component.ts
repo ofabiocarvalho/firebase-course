70fd185b62920e8043b615480f892d32f8e04ad1
ofabiocarvalho <ofabiocarvalho@gmail.com>
2019-08-13 19:38:47 -0300

    firebase storage deep dive

--- src/app/course-dialog/course-dialog.component.ts
+++ src/app/course-dialog/course-dialog.component.ts
@@ -3,6 +3,9 @@ import {Component, Inject, OnInit} from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialogRef } from "@angular/material/dialog";
 import {Course} from "../model/course";
 import {FormBuilder, Validators, FormGroup} from "@angular/forms";
+import { AngularFireStorage } from '@angular/fire/storage';
+import { Observable } from 'rxjs';
+import { concatMap, last } from 'rxjs/operators';
 
 
 @Component({
@@ -17,11 +20,16 @@ export class CourseDialogComponent implements OnInit {
 
     course: Course;
 
+    uploadedPercent$: Observable<number>;
+
+    downloadUrl$: Observable<string>;
+
     constructor(
         private fb: FormBuilder,
         private dialogRef: MatDialogRef<CourseDialogComponent>,
         @Inject(MAT_DIALOG_DATA) course:Course,
-        private coursesService: CoursesService) {
+        private coursesService: CoursesService,
+        private storage : AngularFireStorage) {
         
         this.course = course;
 
@@ -31,6 +39,30 @@ export class CourseDialogComponent implements OnInit {
             description: [titles.description, Validators.required],
             longDescription: [titles.longDescription,Validators.required]
         });
+
+    }
+
+    uploadFile(event){
+        const file: File = event.target.files[0];
+        
+        const filePath = `courses/${this.course.id}/${file.name}`;
+
+        const task = this.storage.upload(filePath, file);
+        
+        this.uploadedPercent$ = task.percentageChanges();
+
+        this.downloadUrl$ = task.snapshotChanges()
+            .pipe(
+                last(), 
+                concatMap(() => this.storage.ref(filePath).getDownloadURL())
+            );
+
+        const saveUrl$ = this.downloadUrl$
+            .pipe(
+                concatMap(url => this.coursesService.saveCourse(this.course.id, {uploadedImageUrl: url}))
+            );
+
+        saveUrl$.subscribe(console.log);
 
     }
 
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyArpXN0lyGSj0_6BamLlOMBz1TVeHgIebQ",
+    authDomain: "fir-course-1f41d.firebaseapp.com",
+    databaseURL: "https://fir-course-1f41d.firebaseio.com",
+    projectId: "fir-course-1f41d",
+    storageBucket: "fir-course-1f41d.appspot.com",
+    messagingSenderId: "612617694077",
+    appId: "1:612617694077:web:b9f28ba479514366"
+  }
 };
