baf5dde88c777961987af10679186944bf6128cb
ofabiocarvalho <ofabiocarvalho@gmail.com>
2019-08-12 21:07:52 -0300

    firebase authentication

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,35 @@
-import { Component } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { Component, OnInit } from '@angular/core';
+import { map } from 'rxjs/operators';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
 
+    isLoggedIn$: Observable<boolean>;
+    isLoggedOut$: Observable<boolean>;
+    pictureUrl$: Observable<string>;
+
+    constructor(private afAuth:AngularFireAuth){
+
+    }
+
+    ngOnInit(){
+      this.afAuth.authState.subscribe(user => console.log(user));
+
+      this.isLoggedIn$ = this.afAuth.authState.pipe(map(user => !!user));
+
+      this.isLoggedOut$ = this.isLoggedIn$.pipe(map(loogedIn => !loogedIn));
+
+      this.pictureUrl$ = this.afAuth.authState.pipe(map(user => user ? user.photoURL : null));
+    }
 
     logout() {
-
+      this.afAuth.auth.signOut();
     }
 
 }
